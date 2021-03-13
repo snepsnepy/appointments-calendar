fde887a40e7f1f3ec9ed740da42108f04eb7c447
unknown <snep_graffiti20@yahoo.com>
2021-03-13 15:58:21 +0200

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main class="app">
+      <Calendar />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calendar from "./components/Calendar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    Calendar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+* {
+  margin: 0;
+}
+
+.app {
+  background-image: url("./assets/background.jpg");
+  background-size: cover;
+  width: 100%;
+  height: auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.use(VueTextareaAutosize)
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDgjzfQyKyCdbw_rR1eInainf3EW2tpo1w",
+  authDomain: "calendarapp-f45ab.firebaseapp.com",
+  databaseURL: "https://calendarapp-f45ab-default-rtdb.firebaseio.com",
+  projectId: "calendarapp-f45ab",
+  storageBucket: "calendarapp-f45ab.appspot.com",
+  messagingSenderId: "447076897501",
+  appId: "1:447076897501:web:ade3af59db819af3138134",
+  measurementId: "G-DSLR4ELNXK"
+});
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
